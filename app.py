9188fe2d5fbb83647e448776c75540e47b3a665e
Gustavo Vituri <gustavovituri@gmail.com>
2023-04-17 11:59:03 -0300

    fix tile coorditate finder

--- app.py
+++ app.py
@@ -23,22 +23,32 @@ foreground_color = sprite_sheet_pixel[full_tile_coordinate_x,full_tile_coordinat
 print(background_color)
 print(foreground_color)
 
-#Initializes the sprite sheet
-sprite_sheet_8x = pygame.image.load('resources/ascii_table_8x.png').convert_alpha()
-sprite_sheet_16x = pygame.image.load('resources/ascii_table_16x.png').convert_alpha()
+def get_tile_from_sheet(sheet_path, tile_index, resolution, scale):
+#Gets the RGB value for background and foreground
+    sprite_sheet = Image.open(sheet_path)
+    sprite_sheet_pixel = sprite_sheet.load()
+    void_tile_coordinate_y = math.trunc(32/16) * resolution
+    void_tile_coordinate_x = (32 - (math.trunc(32/16) * 16)) * resolution
+    full_tile_coordinate_y = math.trunc(219/16) * resolution
+    full_tile_coordinate_x = (219 - (math.trunc(219/16) * 16)) * resolution
+    background_color = sprite_sheet_pixel[void_tile_coordinate_x,void_tile_coordinate_y]
+    foreground_color = sprite_sheet_pixel[full_tile_coordinate_x,full_tile_coordinate_y]
+    print(background_color)
+    print(foreground_color)
 
-def get_tile_from_sheet(sheet, tile_index, resolution, scale, background_color):
+    sprite_sheet_game = pygame.image.load(sheet_path).convert_alpha()
+
     tile = pygame.Surface((resolution, resolution)).convert_alpha()
 
-    tile_coordinate_y = math.trunc((tile_index/16)) * resolution
-    tile_coordinate_x = (tile_index - tile_coordinate_y) * resolution
+    tile_coordinate_y = math.trunc(tile_index/16) * resolution
+    tile_coordinate_x = (tile_index - (math.trunc(tile_index/16) * 16)) * resolution
 
-    tile.blit(sheet, (0, 0), (tile_coordinate_x ,tile_coordinate_y, resolution, resolution))
+    tile.blit(sprite_sheet_game, (0, 0), (tile_coordinate_x ,tile_coordinate_y, resolution, resolution))
     tile = pygame.transform.scale(tile, (resolution * scale, resolution * scale))
     tile.set_colorkey(background_color)
     return tile
 
-test_tile = get_tile_from_sheet(sprite_sheet_16x, 178, 16, 1, (255, 0, 255))
+test_tile = get_tile_from_sheet('resources/ascii_table_16x.png', 178, 16, 1)
 
 run = True
 while run:
