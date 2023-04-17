84127da10301e7abc4cb6ce67e44a2003fd34ec2
Gustavo Vituri <gustavovituri@gmail.com>
2023-04-17 14:14:38 -0300

    add background and foreground coloring

--- app.py
+++ app.py
@@ -11,19 +11,7 @@ pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('ASCII RENDERER')
 
-#Gets the RGB value for background and foreground
-sprite_sheet = Image.open('resources/ascii_table_16x.png')
-sprite_sheet_pixel = sprite_sheet.load()
-void_tile_coordinate_y = math.trunc((32/16)) * 16
-void_tile_coordinate_x = (32 - void_tile_coordinate_y) * 16
-full_tile_coordinate_y = math.trunc((219/16)) * 16
-full_tile_coordinate_x = (219 - full_tile_coordinate_y) * 16
-background_color = sprite_sheet_pixel[void_tile_coordinate_x,void_tile_coordinate_y]
-foreground_color = sprite_sheet_pixel[full_tile_coordinate_x,full_tile_coordinate_y]
-print(background_color)
-print(foreground_color)
-
-def get_tile_from_sheet(sheet_path, tile_index, resolution, scale):
+def get_tile_from_sheet(sheet_path, tile_index, resolution, scale, tile_background_color):
 #Gets the RGB value for background and foreground
     sprite_sheet = Image.open(sheet_path)
     sprite_sheet_pixel = sprite_sheet.load()
@@ -38,6 +26,11 @@ def get_tile_from_sheet(sheet_path, tile_index, resolution, scale):
 
     sprite_sheet_game = pygame.image.load(sheet_path).convert_alpha()
 
+    #adds color to the background
+    tile_background = pygame.Surface((resolution, resolution)).convert_alpha()
+    tile_background.fill(tile_background_color)
+
+    #slice the tile from the spritesheet
     tile = pygame.Surface((resolution, resolution)).convert_alpha()
 
     tile_coordinate_y = math.trunc(tile_index/16) * resolution
@@ -45,10 +38,20 @@ def get_tile_from_sheet(sheet_path, tile_index, resolution, scale):
 
     tile.blit(sprite_sheet_game, (0, 0), (tile_coordinate_x ,tile_coordinate_y, resolution, resolution))
     tile = pygame.transform.scale(tile, (resolution * scale, resolution * scale))
-    tile.set_colorkey(background_color)
-    return tile
 
-test_tile = get_tile_from_sheet('resources/ascii_table_16x.png', 178, 16, 1)
+    #changes the color of the tile
+    tile.set_colorkey(foreground_color)
+    tile_new_color = tile.copy()
+    tile_new_color.fill((255,0,0))
+    tile_new_color.blit(tile, (0,0))
+    tile_new_color.set_colorkey(background_color)
+
+    #add foreground and background colors
+    tile_background.blit(tile_new_color, (0, 0))
+    
+    return tile_background
+
+test_tile = get_tile_from_sheet('resources/ascii_table_16x.png', 178, 16, 1, (170, 255, 238))
 
 run = True
 while run:
